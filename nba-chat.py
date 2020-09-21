fdd911df5d6af87fac7036ade87854e1e13a722c
Kash <amahlw@gmail.com>
2020-09-21 16:27:25 -0400

    created nab bot response
    Create

---
+++ nba-chat.py
@@ -0,0 +1,28 @@
+
+
+from random import choice
+
+#combine functions and conditionals to get a response from the bot
+def nba_finals_bot_response(user_response):
+
+  #add some bot responses to this list
+  bot_response_Lakers = ["Yesir!! KOBE", "Go Lakers!", "Aint nothing like purple an Gold!"]
+  bot_response_Celtics = ["Sheesh i guess!", "Go Clover","Gang Green"]
+  bot_response_Heat = ["305 to my city", "Wade County","Jimmy Buckets!!","Herro Ball"]
+  bot_response_Nuggets = ["The Comeback Kidz","It's teh Joker","About Time👌"]
+
+  if user_response.upper() ==  "LAKERS":
+    return choice(bot_response_Lakers)
+  elif user_response.upper() == "CELTICS":
+    return choice(bot_response_Celtics)
+  elif user_response.upper() == "HEAT":
+    return choice(bot_response_Heat)
+  elif user_response.upper() == "NUGGETS":
+    return choice(bot_response_Nuggets)
+  else:
+    return "Better Luck Next Time😤"
+
+
+
+
+
